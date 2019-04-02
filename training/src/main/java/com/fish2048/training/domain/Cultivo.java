c759e1db7eda193ce998c2c87ca742b4db302de2
AlanMiranda-BR <alan.0.bagualito@gmail.com>
2019-04-02 16:50:12 -0300

    add Cultivo (domain, reposiotories, resources e services)

---
+++ training/src/main/java/com/fish2048/training/domain/Cultivo.java
@@ -0,0 +1,73 @@
+package com.fish2048.training.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cultivo implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String descricao;
+	
+	//construtores
+	public Cultivo() {}
+	
+	public Cultivo(Integer id, String descricao) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+	//getters e setters
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	
+	//hashCode e equals
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cultivo other = (Cultivo) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
