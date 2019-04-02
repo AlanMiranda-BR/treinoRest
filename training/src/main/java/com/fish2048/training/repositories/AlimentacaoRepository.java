253db2566e363f230e2d0e6c779093c696dd5f9d
Rafael <rsneukirchen@gmail.com>
2019-04-02 16:22:25 -0300

    Criei a classe Alimentacao completa e funcional

---
+++ training/src/main/java/com/fish2048/training/repositories/AlimentacaoRepository.java
@@ -0,0 +1,14 @@
+package com.fish2048.training.repositories;
+
+/**
+ * Criado por Rafael Silva Neukirchen
+ */
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.fish2048.training.domain.Alimentacao;
+
+@Repository
+public interface AlimentacaoRepository extends JpaRepository<Alimentacao, Integer>{
+
+}
